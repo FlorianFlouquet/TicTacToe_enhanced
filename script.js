06078511c6663496d4a80be4a34c6aacb82b709a
FlorianFlouquet <florian.flouquet2@hotmail.com>
2022-06-12 14:16:47 +0200

    Added a feature to change username + created a .gitignore file

--- script.js
+++ script.js
@@ -1,10 +1,46 @@
 // DOM elements
 
-const buttons = document.querySelectorAll('#buttonPlaceholder > button');
-const gameboard = document.querySelectorAll('.gameboard');
+const gamemodeButtons = document.querySelectorAll('.gamemode-button-placeholder > button');
+const gameboardButtons = document.querySelectorAll('.gameboard-button-placeholder > button');
+const gameboard = document.querySelectorAll('.gamemode-board');
+const editUsername = document.querySelectorAll('.scoreboard > h2 > span');
 const gamemodeSelectionMenu = document.querySelector('.gamemode-selection');
+const grid = document.querySelector('.grid');
+const scores = document.querySelectorAll('.scoreboard > h3');
+
+// Variables
+let scoreJoueur1 = 0;
+let scoreJoueur2 = 0;
 
 // Fonctions 
+
+const changeUsername = (span) => {
+    let newPseudo = prompt('Entrez votre pseudo');
+    span.parentNode.textContent = newPseudo;
+}
+
+/**
+ * Réinitialise les scores des joueurs
+ */
+const resetScoreboard = () => {
+    console.log('reset');
+}
+
+/**
+ * Génére le grille du morpion
+ */
+const generateGrid = () => {
+    for (let i = 0; i < 3; i++) {
+        const ligne = document.createElement('div');
+        ligne.className = "line";
+        for (let j = 0; j < 3; j++) {
+            const cellule = document.createElement('div');
+            cellule.className = "cell";
+            ligne.appendChild(cellule);
+        }
+        grid.appendChild(ligne);
+    }
+}
 
 /**
  * Cache le menu pour choisir le mode de jeu.
@@ -16,7 +52,7 @@ const initializeGame = (index) => {
     // On cache le menu de séléction des modes de jeu
     gamemodeSelectionMenu.style.display = "none";
     // On fait apparaître le plateau de jeu qui correspond au mode de jeu choisi
-    gameboard[index].style.display = "block";
+    gameboard[index].style.display = "flex";
 }
 
 // EventListeners
@@ -24,8 +60,21 @@ const initializeGame = (index) => {
 // Pour chaque bouton dans le menu de selection des modes de jeu,
 // on lui prend son index et on lui met un eventListener qui appelle
 // la fonction initializeGame avec en paramètre l'index du bouton
-buttons.forEach((button, index) => {
-    button.addEventListener('mouseup', () => {
+gamemodeButtons.forEach((button, index) => {
+    button.addEventListener('click', () => {
         initializeGame(index);
     });
 });
+
+gameboardButtons[1].addEventListener('click', resetScoreboard);
+
+editUsername.forEach((span) => {
+    span.addEventListener('click', (event) => {
+        changeUsername(event.target);
+    })
+})
+
+
+// Fonction Déclaré Temporairement
+
+generateGrid();
